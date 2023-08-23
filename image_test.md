8a89e95dad7ed1d3e992fede5df1aedc2acab425
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-08-23 10:25:17 +0200

    more images examples

--- image_test.md
+++ image_test.md
@@ -1,10 +1,24 @@
 # Images
 
-## svg
+## SVG
 
-![](images/example.svg)
+Example images
+
+![An svg example](images/example.svg)
+
+a4 with gradient
+
+![An A4 gradient example](images/a4_gradient.svg)
 
 ## jpg
+
+![x](images/a4_gradient.svg)
+![x](images/animation.gif)
+![x](images/example.svg)
+![x](images/jpg-vs-jpeg-1024x512.jpg)
+![x](images/logo_rawworks_text.webp)
+![x](images/PNG_transparency_demonstration_1.png)
+![x](images/rawworks-share.jpg)
 
 ## png
 
--- image_test.sh
+++ image_test.sh
@@ -2,6 +2,18 @@
 
 echo 'Using image_test.sh to convert .md 2 .docx'
 
+echo 'Step 0. Close LibreOffice'
+echo '(it seems impossible to refresh a new document with the same name)'
+osascript -e 'tell application "LibreOffice" to quit'
+# osascript -e 'tell application "Word" to quit'
+
+
+
 echo 'Step 1: Convert markdown to docx'
 pandoc -o image_test.docx -f markdown -t docx image_test.md
 # pandoc -o output.pdf test.md
+
+
+echo 'Step 2: Open docx in LibreOffice'
+# open image_test.docx
+open -a "LibreOffice" image_test.docx
